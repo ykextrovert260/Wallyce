fb824555a5d6649effa98f364f3ec368bd35cd80
yk <95935220+ykextrovert260@users.noreply.github.com>
2024-01-04 03:50:49 +0530

    Update format.sh

--- .github/format.sh
+++ .github/format.sh
@@ -17,6 +17,6 @@ done
 if [[ -n "$(git status | grep "json")" ]]; then
     git add .
     git commit --amend -m "${COMMIT_MESSAGE}" --author="${COMMIT_AUTHOR}" --signoff
-    git remote set-url origin "https://ykextrovert260:${GH_TOKEN}@github.com/ykextrovert260/wallyce_json/"
+    git remote set-url origin "https://ykextrovert260:${GH_TOKEN}@github.com/ykextrovert260/wallyce/"
     git push origin -f
 fi
